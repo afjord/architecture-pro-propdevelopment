868cc0b697fc3d4a2037202ff350914b51062be9
afjord <andreyfedorov43@gmail.com>
2026-03-22 22:03:17 +0300

    Task5: add pods creation

---
+++ Task5/create_pods.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+kubectl create namespace development
+
+kubectl run front-end-app --image=nginx --labels role=front-end --expose --port 80 -n development
+kubectl run back-end-api-app --image=nginx --labels role=front-end --expose --port 80 -n development
+kubectl run admin-front-end-app --image=nginx --labels role=front-end --expose --port 80 -n development
+kubectl run admin-back-end-api-app --image=nginx --labels role=front-end --expose --port 80 -n development
